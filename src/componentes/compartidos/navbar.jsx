8a2655e546422adb9519bf25369abcb918476337
YareliHH <yhh122004@gmail.com>
2024-06-14 12:38:02 -0600

    add:estadia

--- src/componentes/compartidos/navbar.jsx
+++ src/componentes/compartidos/navbar.jsx
@@ -5,22 +5,24 @@ import Logo from '../Imagenes/CapLogo.png';
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
+
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
+
   return (
     <nav className="navbar is-white" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
         <Link className="navbar-item" to="/">
           <img src={Logo} alt="CapLogo" />
-         </Link>
+        </Link>
         <div className={navbar-burger burger ${showMenu ? 'is-active' : ''}} onClick={toggleMenu}>
           <span></span>
           <span></span>
           <span></span>
         </div>
       </div>
-      
+
       <div className={navbar-menu ${showMenu ? 'is-active' : ''}}>
         <div className="navbar-start">
           <Link className="navbar-item" to="/" style={{ color: 'black' }}>
